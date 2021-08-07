1e0de6c2af48fcb5477def2d629265be330c1289
Ivan GC <ivanchav112@gmail.com>
2021-08-07 15:52:25 -0500

    all modules were imported in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,13 @@ import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { UsersModule } from './users/users.module';
 import { RolesGuard } from './auth/roles.guard';
+import { CompaniesModule } from './companies/companies.module';
+import { ReportsModule } from './reports/reports.module';
+import { BranchesModule } from './branches/branches.module';
+import { TablesModule } from './tables/tables.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { BranchesUsersModule } from './branches-users/branches-users.module';
 
 @Module({
   imports: [
@@ -19,6 +26,13 @@ import { RolesGuard } from './auth/roles.guard';
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_NAME}?retryWrites=true&w=majority`,
     ),
+    CompaniesModule,
+    ReportsModule,
+    BranchesModule,
+    TablesModule,
+    OrdersModule,
+    ProductsModule,
+    BranchesUsersModule,
   ],
   controllers: [AppController],
   providers: [
