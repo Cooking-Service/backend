8938700d54e9e7fe4e1897525e599961f2f5f65e
Ivan GC <ivanchav112@gmail.com>
2021-08-11 21:01:46 -0500

    findCompanyById added to use on usersService

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { ResponseDto } from 'src/common/dto/response.dto';
 import { Company, CompanyDocument } from './schemas/company.schema';
 
 @Injectable()
@@ -8,4 +9,22 @@ export class CompaniesService {
   constructor(
     @InjectModel(Company.name) private companyModel: Model<CompanyDocument>,
   ) {}
+
+  /**
+   * 
+   */
+  async findById(id: any): Promise<ResponseDto<Company>> {
+    const company = await this.companyModel.findById(id);
+
+    if (!company) {
+      return {
+        success: false,
+      };
+    }
+
+    return {
+      success: true,
+      response: company,
+    };
+  }
 }
