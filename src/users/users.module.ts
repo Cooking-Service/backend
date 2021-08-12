87bb4f56a26ec334e024d118b8f6497b492d3219
Ivan GC <ivanchav112@gmail.com>
2021-08-11 21:05:05 -0500

    imported user hash service and schema

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,17 +3,25 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { CompaniesModule } from 'src/companies/companies.module';
+import { UserHashSchema, UserHash } from './schemas/user-hash.schema';
+import { UsersHashesService } from './users-hashes.service';
 
 @Module({
   imports: [
+    CompaniesModule,
     MongooseModule.forFeature([
       {
         name: User.name,
         schema: UserSchema,
       },
+      {
+        name: UserHash.name,
+        schema: UserHashSchema,
+      },
     ]),
   ],
-  providers: [UsersService],
+  providers: [UsersService, UsersHashesService],
   controllers: [UsersController],
   exports: [UsersService],
 })
