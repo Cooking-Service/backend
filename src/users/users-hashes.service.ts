5d8e622e15a77d57b13543e3223b8d38aa7c2b37
Ivan GC <ivanchav112@gmail.com>
2021-08-11 21:03:19 -0500

    created functionalitiy to generate user hash to confirm and change password

---
+++ src/users/users-hashes.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+import * as mongoose from 'mongoose';
+import { Model } from 'mongoose';
+import { ResponseDto } from 'src/common/dto/response.dto';
+import {
+    HashType,
+    UserHash,
+    UserHashDocument
+} from './schemas/user-hash.schema';
+import { User } from './schemas/user.schema';
+
+@Injectable()
+export class UsersHashesService {
+  constructor(
+    @InjectModel(UserHash.name) private userHashModel: Model<UserHashDocument>,
+    @InjectConnection() private connection: mongoose.Connection,
+  ) {}
+
+  async generateHash(
+    user: User,
+    type: HashType,
+    session?: mongoose.ClientSession,
+  ): Promise<ResponseDto<UserHashDocument>> {
+    const userhash = new this.userHashModel();
+
+    userhash.hash = bcrypt.hashSync(user.email, 10);
+    userhash.createdOn = new Date();
+    userhash.user = user;
+    userhash.type = type;
+
+    if (session) {
+      return {
+        success: true,
+        response: await userhash.save({ session }),
+      };
+    }
+
+    return {
+      success: true,
+      response: await userhash.save(),
+    };
+  }
+}
